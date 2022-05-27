d94652164d533ce17c9b0786a85c8488b9e76ad7
ricardospalves <ricardospalves@gmail.com>
2022-05-27 00:11:11 -0300

    add Footer om home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import type { GetStaticProps, NextPage } from 'next'
 import Head from 'next/head'
 
+import { Footer } from 'src/components/Footer'
+
 const Home: NextPage = () => {
   return (
     <div className="min-h-screen flex flex-col text-black bg-white dark:text-white dark:bg-neutral-900">
@@ -11,6 +13,8 @@ const Home: NextPage = () => {
       <main className="grow">
         <h1 className="font-bold">React Weather</h1>
       </main>
+
+      <Footer />
     </div>
   )
 }
