3094c64efc6bece6f480fcc0915eb2065f8d4b39
ricardospalves <ricardospalves@gmail.com>
2022-05-30 23:14:01 -0300

    fix Base component

--- src/components/Base/Base.tsx
+++ src/components/Base/Base.tsx
@@ -1,20 +1,13 @@
 import type { FC, ReactNode } from 'react'
-import { useContext } from 'react'
-
-import { DarkThemeContext } from 'src/context/DarkThemeContext'
 
 type Props = {
   children: ReactNode
 }
 
 export const Base: FC<Props> = ({ children }) => {
-  const { isDark } = useContext(DarkThemeContext)
-
   return (
-    <div className={isDark ? 'dark' : ''}>
-      <div className="min-h-screen flex flex-col text-black bg-white dark:text-white dark:bg-neutral-900">
-        {children}
-      </div>
+    <div className="min-h-screen flex flex-col text-black bg-white dark:text-white dark:bg-neutral-900">
+      {children}
     </div>
   )
 }
