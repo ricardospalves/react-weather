083c23657330e3de9318eff8d7836c5b3ab55b8e
ricardospalves <ricardospalves@gmail.com>
2022-06-02 23:30:14 -0300

    add author and HtmlLink componet on Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,17 +1,18 @@
 import { GITHUB } from 'src/constants'
+import { GENERAL } from 'src/constants'
+import { HtmlLink } from '../HtmlLink'
 
 export const Footer = () => {
   return (
     <footer className="shrink-0 p-4 text-center">
       <p>
+        Desenvolvido por{' '}
+        <HtmlLink href={GITHUB.userUrl}>{GENERAL.author}</HtmlLink>.
+      </p>
+
+      <p className="mt-4">
         Distribuído sob a{' '}
-        <a
-          href={GITHUB.licenseUrl}
-          className="underline text-blue-700 hover:text-blue-900 focus-visible:text-blue-900 dark:text-blue-500 dark:hover:text-blue-300 dark:focus-visible:text-blue-300 transition-colors"
-        >
-          licença {GITHUB.license}
-        </a>
-        .
+        <HtmlLink href={GITHUB.licenseUrl}>licença {GITHUB.license}</HtmlLink>.
       </p>
     </footer>
   )
