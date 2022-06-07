7efd4fc18360cafbf53d403f5ed04cc157de722d
ricardospalves <ricardospalves@gmail.com>
2022-06-07 00:08:08 -0300

    add className prop t HtmlLink

--- src/components/HtmlLink/HtmlLink.tsx
+++ src/components/HtmlLink/HtmlLink.tsx
@@ -1,13 +1,17 @@
 import type { FC, HTMLProps, ReactNode } from 'react'
+import classNames from 'classnames'
 
 type Props = HTMLProps<HTMLAnchorElement> & {
   children: ReactNode
 }
 
-export const HtmlLink: FC<Props> = ({ children, ...rest }) => {
+export const HtmlLink: FC<Props> = ({ children, className, ...rest }) => {
   return (
     <a
-      className="underline text-blue-700 hover:text-blue-900 focus-visible:text-blue-900 dark:text-blue-500 dark:hover:text-blue-300 dark:focus-visible:text-blue-300 transition-colors"
+      className={classNames([
+        'underline text-blue-700 hover:text-blue-900 focus-visible:text-blue-900 dark:text-blue-500 dark:hover:text-blue-300 dark:focus-visible:text-blue-300 transition-colors',
+        className,
+      ])}
       {...rest}
     >
       {children}
