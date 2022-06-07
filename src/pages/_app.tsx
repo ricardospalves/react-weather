3a3218d44e97e47a5d2357a011ac5f22c2090245
ricardospalves <ricardospalves@gmail.com>
2022-06-07 00:08:54 -0300

    fix style import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app'
 
-import 'styles/index.css'
+import 'src/styles/index.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
