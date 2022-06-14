fba38c7836b8457b32a3568e4962c591fb4b6539
ricardospalves <ricardospalves@gmail.com>
2022-06-13 23:30:31 -0300

    add TemperatureUnit component

---
+++ src/components/TemperatureUnit/TemperatureUnit.tsx
@@ -0,0 +1,26 @@
+import { pluralize } from 'src/helpers/pluralize'
+import { Abbr } from '../Abbr'
+
+type Props = {
+  temperature: number
+  removeDecoration?: boolean
+}
+
+export const TemperatureUnit = ({ temperature, removeDecoration }: Props) => {
+  const title = `${Math.abs(temperature)} ${pluralize(temperature, {
+    singular: 'grau',
+  })} Celsius`
+  const negativeTitle = `${title} ${pluralize(temperature, {
+    singular: 'negativo',
+  })}`
+  const isNegative = temperature < 0
+
+  return (
+    <Abbr
+      title={isNegative ? negativeTitle : title}
+      removeDecoration={removeDecoration}
+    >
+      {temperature}°C
+    </Abbr>
+  )
+}
