ee47d3b4e0c3a80ef8b8c6011291d2afc73260d4
ricardospalves <ricardospalves@gmail.com>
2022-07-01 00:18:05 -0300

    add getCurrentWeatherData api

---
+++ src/service/api/getCurrentWeatherData.ts
@@ -0,0 +1,71 @@
+import { apiClient } from './apiClient'
+
+type Coordinates = {
+  lon: number
+  lat: number
+}
+
+type Weather = {
+  id: number
+  main: string
+  description: string
+  icon: string
+}
+
+type Main = {
+  temp: number
+  feels_like: number
+  temp_min: number
+  temp_max: number
+  pressure: number
+  humidity: number
+}
+
+type Wind = {
+  speed: number
+  deg: number
+}
+
+type Clouds = {
+  all: number
+}
+
+type Sys = {
+  type: number
+  id: number
+  country: string
+  sunrise: number
+  sunset: number
+}
+
+export type GetCurrentWeatherDataResponseData = {
+  coord: Coordinates
+  weather: Weather[]
+  base: string
+  main: Main
+  visibility: number
+  wind: Wind
+  clouds: Clouds
+  dt: number
+  sys: Sys
+  timezone: number
+  id: number
+  name: string
+  cod: number
+}
+
+export type GetCurrentWeatherDataProps = {
+  q: string
+}
+
+export const getCurrentWeatherData = async ({
+  q,
+}: GetCurrentWeatherDataProps) => {
+  const response = await apiClient.get('weather', {
+    params: {
+      q,
+    },
+  })
+
+  return response
+}
