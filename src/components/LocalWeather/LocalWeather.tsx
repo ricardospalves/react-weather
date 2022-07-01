45daf26fd26d00f5a57e34637b6fe10fbefd211a
ricardospalves <ricardospalves@gmail.com>
2022-07-01 00:29:01 -0300

    add data from api

--- src/components/LocalWeather/LocalWeather.tsx
+++ src/components/LocalWeather/LocalWeather.tsx
@@ -1,28 +1,44 @@
+import { useContext } from 'react'
+import { format } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+
+import { CurrentWeatherDataContext } from 'src/contexts/CurrentWeatherDataContext'
 import { TemperatureUnit } from '../TemperatureUnit'
 import { SpeedUnit } from '../SpeedUnit'
 import { ComplementaryWeather } from '../ComplementaryWeather'
 
-type Props = {
-  temperature: number
-}
+export const LocalWeather = () => {
+  const {
+    query: { data },
+  } = useContext(CurrentWeatherDataContext)
 
-export const LocalWeather = ({ temperature }: Props) => {
+  if (!data) {
+    return null
+  }
+
+  const date = new Date(data?.dt * 1000)
+
   return (
     <article className="mt-4 p-4">
       <h2 className="font-bold text-2xl text-center">
-        O tempo agora em Lorem Ipsum, XX
+        O tempo agora em {data?.name}, {data?.sys.country}
       </h2>
 
-      <p className="text-center mt-2">Sexta-feira, 00 de junho de 2022.</p>
+      <p className="text-center mt-2">
+        {format(date, `EEEE, d 'de' MMMM 'de' yyyy, HH:mm`, {
+          locale: ptBR,
+        })}
+        .
+      </p>
 
-      <p className="text-center mt-8 mb-1">Lorem ipsum dolor.</p>
+      <p className="text-center mt-8 mb-1">{data.weather[0].description}</p>
 
       <h3 className="text-center text-8xl font-bold">
-        <TemperatureUnit temperature={temperature} removeDecoration />
+        <TemperatureUnit temperature={data.main.temp} removeDecoration />
       </h3>
 
       <p className="text-center mt-2">
-        Sensação de <TemperatureUnit temperature={temperature} />.
+        Sensação de <TemperatureUnit temperature={data.main.feels_like} />.
       </p>
 
       <ComplementaryWeather
@@ -30,11 +46,11 @@ export const LocalWeather = ({ temperature }: Props) => {
         list={[
           {
             title: 'Vento',
-            description: <SpeedUnit speed={0} />,
+            description: <SpeedUnit speed={data.wind.speed * 3.6} />,
           },
           {
             title: 'Umidade',
-            description: '0%',
+            description: `${data.main.humidity}%`,
           },
         ]}
       />
