086767069fa61ae4fabeda52362ac1d419c9eae6
ricardospalves <ricardospalves@gmail.com>
2022-07-07 23:42:30 -0300

    add Skeleton to LocalWeather

--- src/components/LocalWeather/LocalWeather.tsx
+++ src/components/LocalWeather/LocalWeather.tsx
@@ -6,17 +6,31 @@ import { CurrentWeatherDataContext } from 'src/contexts/CurrentWeatherDataContex
 import { TemperatureUnit } from '../TemperatureUnit'
 import { SpeedUnit } from '../SpeedUnit'
 import { ComplementaryWeather } from '../ComplementaryWeather'
+import { Skeleton } from '../Skeleton'
 
 export const LocalWeather = () => {
   const {
-    query: { data },
+    query: { data, isFetching },
   } = useContext(CurrentWeatherDataContext)
+
+  if (isFetching) {
+    return (
+      <div className="mt-4 p-4">
+        <Skeleton className="h-8" />
+        <Skeleton className="mt-2 h-6" />
+        <Skeleton className="mt-8 mb-1 h-6" />
+        <Skeleton className="h-24" />
+        <Skeleton className="mt-2 h-6" />
+        <Skeleton className="h-14 mt-8" />
+      </div>
+    )
+  }
 
   if (!data) {
     return null
   }
 
-  const date = new Date(data?.dt * 1000)
+  const date = data?.dt && new Date(data.dt * 1000)
 
   return (
     <article className="mt-4 p-4">
@@ -25,20 +39,22 @@ export const LocalWeather = () => {
       </h2>
 
       <p className="text-center mt-2">
-        {format(date, `EEEE, d 'de' MMMM 'de' yyyy, HH:mm`, {
-          locale: ptBR,
-        })}
+        {date &&
+          format(date, `EEEE, d 'de' MMMM 'de' yyyy, HH:mm`, {
+            locale: ptBR,
+          })}
         .
       </p>
 
-      <p className="text-center mt-8 mb-1">{data.weather[0].description}</p>
+      <p className="text-center mt-8 mb-1">{data?.weather[0].description}</p>
 
       <h3 className="text-center text-8xl font-bold">
-        <TemperatureUnit temperature={data.main.temp} removeDecoration />
+        <TemperatureUnit temperature={data?.main.temp || 0} removeDecoration />
       </h3>
 
       <p className="text-center mt-2">
-        Sensação de <TemperatureUnit temperature={data.main.feels_like} />.
+        Sensação de <TemperatureUnit temperature={data?.main.feels_like || 0} />
+        .
       </p>
 
       <ComplementaryWeather
@@ -46,11 +62,13 @@ export const LocalWeather = () => {
         list={[
           {
             title: 'Vento',
-            description: <SpeedUnit speed={data.wind.speed * 3.6} />,
+            description: (
+              <SpeedUnit speed={(data && data?.wind.speed * 3.6) || 0} />
+            ),
           },
           {
             title: 'Umidade',
-            description: `${data.main.humidity}%`,
+            description: `${data?.main.humidity || 0}%`,
           },
         ]}
       />
