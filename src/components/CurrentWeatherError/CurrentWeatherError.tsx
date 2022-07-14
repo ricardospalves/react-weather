02971d4c529c597fa4bfbed2bb621ea36c966274
ricardospalves <ricardospalves@gmail.com>
2022-07-13 23:38:27 -0300

    add styled to CurrentWeatherError

--- src/components/CurrentWeatherError/CurrentWeatherError.tsx
+++ src/components/CurrentWeatherError/CurrentWeatherError.tsx
@@ -1,3 +1,5 @@
+import { MdError as ErrorIcon } from 'react-icons/md'
+
 type CurrentWeatherErrorProps = {
   heading: string
   description: string
@@ -8,7 +10,12 @@ export const CurrentWeatherError = ({
   heading,
 }: CurrentWeatherErrorProps) => {
   return (
-    <section className="text-center max-w-sm mx-auto mt-8">
+    <section className="rounded p-4 text-center max-w-sm mx-auto mt-8 border bg-red-400 border-red-600 dark:bg-red-600 dark:border-red-800">
+      <ErrorIcon
+        className="block w-12 h-12 fill-current mx-auto"
+        aria-hidden={true}
+      />
+
       <h2 className="font-bold mb-4 text-3xl">{heading}</h2>
 
       <p>{description}</p>
