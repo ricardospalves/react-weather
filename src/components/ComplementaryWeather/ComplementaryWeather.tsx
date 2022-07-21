02b3e40560eff9f16bc4854a715489e604b6532b
ricardospalves <ricardospalves@gmail.com>
2022-07-21 00:39:53 -0300

    add Icon prop to ComplementaryWeather

--- src/components/ComplementaryWeather/ComplementaryWeather.tsx
+++ src/components/ComplementaryWeather/ComplementaryWeather.tsx
@@ -1,10 +1,13 @@
 import type { ReactNode } from 'react'
 import { Fragment } from 'react'
 import classNames from 'classnames'
+import { IconBase } from 'react-icons'
 
 type ComplementaryProps = {
   title: string
   description: ReactNode
+  Icon?: typeof IconBase
+  iconClassName?: string
 }
 
 type Props = {
@@ -15,13 +18,30 @@ type Props = {
 export const ComplementaryWeather = ({ list, className }: Props) => {
   return (
     <dl className={classNames(['flex justify-center flex-wrap', className])}>
-      {list.map(({ description, title }) => {
+      {list.map(({ description, title, Icon, iconClassName }) => {
         return (
           <Fragment key={title}>
             <dt className="font-bold basis-1/2 shrink-0 p-1 text-right">
               {title}
             </dt>
-            <dd className="basis-1/2 shrink-0 text-left p-1">{description}</dd>
+
+            <dd
+              className={classNames([
+                'basis-1/2 shrink-0 text-left p-1',
+                Icon && 'flex items-center',
+              ])}
+            >
+              {description}
+
+              {Icon && (
+                <Icon
+                  className={classNames([
+                    'shrink-0 w-6 h-6 ml-1',
+                    iconClassName,
+                  ])}
+                />
+              )}
+            </dd>
           </Fragment>
         )
       })}
