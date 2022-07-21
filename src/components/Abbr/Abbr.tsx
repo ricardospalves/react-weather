9270a4bbef572cd175e63a363c638e2639dac95b
ricardospalves <ricardospalves@gmail.com>
2022-07-21 00:38:54 -0300

    add className prop to Abbr component

--- src/components/Abbr/Abbr.tsx
+++ src/components/Abbr/Abbr.tsx
@@ -5,15 +5,22 @@ type Props = {
   children: ReactNode
   title: string
   removeDecoration?: boolean
+  className?: string
 }
 
-export const Abbr = ({ children, title, removeDecoration }: Props) => {
+export const Abbr = ({
+  children,
+  title,
+  removeDecoration,
+  className,
+}: Props) => {
   return (
     <abbr
       title={title}
       className={classNames([
         'cursor-help',
         removeDecoration && 'no-underline',
+        className,
       ])}
     >
       {children}
